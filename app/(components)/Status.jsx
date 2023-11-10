88fce0f5ff232b07f313ef9c8e8e98b5eb0d326c
Rajiv S <rajiv620019@gmail.com>
2023-11-10 12:14:38 +0530

    Added components

---
+++ app/(components)/Status.jsx
@@ -0,0 +1,8 @@
+const Status = () => {
+  return (
+    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-300">
+      Done
+    </span>
+  );
+};
+export default Status;
--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,11 +1,15 @@
 import DeleteBlock from "./DeleteBlock";
 import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+import Status from "./Status";
 
 const TicketCard = () => {
   return (
     <div>
       <DeleteBlock />
       <PriorityDisplay />
+      <ProgressDisplay />
+      <Status />
     </div>
   );
 };
